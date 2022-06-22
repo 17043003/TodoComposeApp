c03cd0f5714acf2a6b1d39d6467c98cba98eb4e3
17043003 <ishizuka533725@gmail.com>
2022-06-22 20:39:09 +0900

    add annotation to todo model to use on room.

--- app/src/main/java/com/ishzk/android/todocomposeapp/model/Todo.kt
+++ app/src/main/java/com/ishzk/android/todocomposeapp/model/Todo.kt
@@ -1,7 +1,12 @@
 package com.ishzk.android.todocomposeapp.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
 data class Todo(
+    @PrimaryKey(autoGenerate = true) val id: Int,
     val title: String,
-    val completed: Boolean = false,
+    val done: Boolean = false,
     val deadline: Long,
 )
