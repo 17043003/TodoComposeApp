488d4612d971d0af1836ccf747df5c2e2185140e
17043003 <ishizuka533725@gmail.com>
2022-06-26 11:49:39 +0900

    id column is nullable to auto generate.

--- app/src/main/java/com/ishzk/android/todocomposeapp/model/Todo.kt
+++ app/src/main/java/com/ishzk/android/todocomposeapp/model/Todo.kt
@@ -5,7 +5,7 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class Todo(
-    @PrimaryKey(autoGenerate = true) val id: Int,
+    @PrimaryKey(autoGenerate = true) val id: Int? = null,
     val title: String,
     val done: Boolean = false,
     val deadline: Long,
